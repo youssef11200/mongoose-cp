0d563f3d71e5e54cd50fc6bd13b93be443aba0d2
youssef nasser <youssefnasser261@gmail.com>
2022-10-04 12:42:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const Person = require("./models/person");
+
+Person.insertMany([{
+    name:"youssef nasser",
+    age:25,
+    favoriteFoods:["favFoodOne","favFoodTwo","favFoodThree"]
+}]).then( insertedElement => {console.log("The Inserted Elemenmt: ",insertedElement)});
+
+Person.create([
+    {
+        name:"Person X",
+        age:34,
+        favoriteFoods:["favFoodFour","favFoodFive"]
+    },
+    {
+        name:"Person Y",
+        age:43,
+        favoriteFoods:["favFoodSix","favFoodSeven"]
+    },
+    {
+        name:"Person Z",
+        age:19,
+        favoriteFoods:["favFoodEight","favFoodNine"]
+    }
+]).then(insertedElement => {console.log("The Inserted Elemenmt: ",insertedElement)});
+
+
+
+Person.find().then(remainingPerson => {console.log(remainingPerson)});
+
+
+Person.findOne().then(remainingPerson => {console.log(remainingPerson)});
+
+
+
+
+Person.findById(id, function (err, docs) {console.log(docs)});
+
+
+Person.findOneAndUpdate({"name":"youssef nasser"},{$set:{age:"26"}})
+.then(updatedPerson => {console.log("Updated Person: ",updatedPerson)});
+
+
+
+
+Person.findByIdAndRemove(id, function (err, docs) {console.log("Removed Person :",docs)});
+
+
+
+Person.remove({name:"youssef nasser"})
+.then(removedPersons => {console.log("Removed Person :",removedPersons)});
---
+++ models/person.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const connection = require("../util/database");
+
+const person = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    age: Number,
+    favoriteFoods:[String]
+});
+
+
+const Person = connection.model("Person",person);
+module.exports = Person;
